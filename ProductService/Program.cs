2c4c24540a0fd72f2b9bb9bcf37cd19c318d5196
Diogo Conceição <diogoncmc@gmail.com>
2025-06-03 17:21:30 +0100

    clean code

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -12,27 +12,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddSingleton<IMongoClient>(sp =>
-//{
-//    var logger = sp.GetRequiredService<ILoggerFactory>().CreateLogger("MongoClientInitialization");
-//    var connectionString = builder.Configuration.GetValue<string>("MongoDb:ConnectionString")
-//                           ?? "mongodb://localhost:27017";
-//    try
-//    {
-//        var client = new MongoClient(connectionString);
-//        // Health check ping
-//        var database = client.GetDatabase("admin");
-//        database.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait();
-//        logger.LogInformation("✅ Successfully connected to MongoDB.");
-//        return client;
-//    }
-//    catch (Exception ex)
-//    {
-//        logger.LogError(ex, "❌ Failed to connect to MongoDB.");
-//        throw;
-//    }
-//});
-
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IMongoClient, MongoClient>();
